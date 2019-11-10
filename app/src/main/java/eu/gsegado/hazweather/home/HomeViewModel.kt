4b9b3f45bfe79f20e0d2768a60b0e6818dfbdbb9
GuillaumeSeg <guillaume.segado@gmail.com>
2019-11-10 16:16:01 +0100

    NF - Design Header home

--- app/src/main/java/eu/gsegado/hazweather/home/HomeViewModel.kt
+++ app/src/main/java/eu/gsegado/hazweather/home/HomeViewModel.kt
@@ -6,9 +6,14 @@ import eu.gsegado.hazweather.repository.WeatherRepository
 
 class HomeViewModel : ViewModel() {
 
+    private var latitude: Double? = null
+    private var longitude: Double? = null
+
     private val weatherRepository = WeatherRepository()
 
     fun requestWeather(lat: Double, lng: Double) {
+        latitude = lat
+        longitude = lng
         weatherRepository.fetch(lat,lng)
         Log.d("YOLO", "lat : "+ lat +"lng : "+ lng)
     }
